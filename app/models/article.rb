a8bfdca6f7a4c63398a5df60ac343b1507a3e98d
SSZZKK <michael.suzuki@aol.com>
2021-09-30 00:10:33 +0000

    動画機能の実装

---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+class Article < ApplicationRecord
+    validates :title, :video, :presence => true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :articles
   get 'sessions/new'
 
   root 'pages#index'
