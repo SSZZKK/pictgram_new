d13a0dbd50c702f8bb71a05584f41b27e8b4615d
SSZZKK <michael.suzuki@aol.com>
2021-10-18 22:29:08 +0000

    movieのdescriotionの記述をコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :articles
   get 'sessions/new'
 
   root 'pages#index'
@@ -14,6 +13,7 @@ Rails.application.routes.draw do
   resources :users
   resources :topics
   resources :comments
+  resources :articles
 
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
